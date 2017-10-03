cc7552ec20274ba2fb196f36929078b09eaaf357
areefhida <areef.hiide@gmail.com>
2017-10-03 16:25:24 +0700

    all

--- LoginApp/activity/MainActivity.cs
+++ LoginApp/activity/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using Android.Support.V7.App;
+using LoginApp.fragment;
 
 namespace LoginApp
 {
@@ -13,13 +14,22 @@ namespace LoginApp
     public class MainActivity : AppCompatActivity
     {
 
+        private Button btnSignUp;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             //Set your main view here
             SetContentView(Resource.Layout.main);
+            btnSignUp = FindViewById<Button>(Resource.Id.btnSignUp);
+            btnSignUp.Click += BtnSignUp_Click;
+        }
 
+        private void BtnSignUp_Click(object sender, EventArgs e)
+        {
+            FragmentTransaction transaction = FragmentManager.BeginTransaction();
+            Dialog_SignUp dialog = new Dialog_SignUp();
+            dialog.Show(transaction, "Dialog");            
         }
     }
 }
---
+++ LoginApp/fragment/dialog_SignUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace LoginApp.fragment
+{
+    class Dialog_SignUp : DialogFragment
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            base.OnCreateView(inflater, container, savedInstanceState);
+            var view = inflater.Inflate(Resource.Layout.dialog_signup, container, false);
+            return view;
+        }
+
+        public override void OnActivityCreated(Bundle savedInstanceState)
+        {
+            base.OnActivityCreated(savedInstanceState);
+        }
+    }
+}
